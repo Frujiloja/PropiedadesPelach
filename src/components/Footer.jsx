da4394789d97ec533a9e1a327a0dedf213906193
Martin Fuks <martinfukss@gmail.com>
2024-10-14 14:42:50 -0300

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,7 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-
   const phoneNumber = "5491152280786"; // Reemplaza con el número de teléfono real
-
 
   const handleClick = () => {
     const message = `Hola, me interesa más información.`;
@@ -19,15 +17,18 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.container}>
         <p className={styles.text}>
-        <a href="mailto:horaciopelach@gmail.com" className={styles.contact}>
-        horaciopelach@gmail.com
+          <a href="mailto:horaciopelach@gmail.com" className={styles.contact}>
+            horaciopelach@gmail.com
           </a>{" "}
-          | <a className={styles.contact} target="_blank" href="https://api.whatsapp.com/send/?phone=5491152280786&text=Hola%2C+me+interesa+m%C3%A1s+informaci%C3%B3n+sobre+una+propiedad.&type=phone_number&app_absent=0">11 5228-0786</a>
-        </p>
-      </div>
-      <div className={styles.container}>
-        <p className={styles.text}>
-          © 2024 Martin Fuks |{" "}
+          |{" "}
+          <a
+            className={styles.contact}
+            target="_blank"
+            href="https://api.whatsapp.com/send/?phone=5491152280786&text=Hola%2C+me+interesa+m%C3%A1s+informaci%C3%B3n+sobre+una+propiedad.&type=phone_number&app_absent=0"
+          >
+            11 5228-0786
+          </a>{" "}
+          |
           <a href="mailto:horaciopelach@gmail.com" className={styles.contact}>
             Contacto
           </a>{" "}
@@ -49,6 +50,11 @@ const Footer = () => {
           >
             Instagram
           </a>
+        </p>
+      </div>
+      <div className={styles.container}>
+        <p className={styles.textv2}>
+          © 2024 Creado por Martin Fuks
         </p>
       </div>
     </footer>
