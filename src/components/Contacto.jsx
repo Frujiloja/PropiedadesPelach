8df84779b68930ac5dd435cfd44ef9908b91b465
Martin Fuks <martinfukss@gmail.com>
2024-10-30 18:34:50 -0300

    email

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -3,8 +3,7 @@ import React, { useState } from "react";
 import edificios from "../assets/vecteezy_city-background-illustration-black_22227434.png";
 import logo from "../assets/only logo.png";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import emailjs from 'emailjs-com';
-
+import emailjs from "emailjs-com";
 
 const Contacto = () => {
   const position = [-34.572766, -58.421053]; // Coordenadas de Buenos Aires (ejemplo)
@@ -16,6 +15,8 @@ const Contacto = () => {
     comment: "",
   });
 
+  const destinatario = "horaciopelach@gmail.com";
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -25,17 +26,32 @@ const Contacto = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.send('service_fcnnvtf', 'template_bwaptb9', formData, '1sBpeKaGLYovogvqY')
-    .then((result) => {
-      alert('Correo enviado con éxito!');
-      console.log(result.text);
-      // Limpia el formulario
-      setFormData({ name: '', email: '', message: '' });
-    }, (error) => {
-      alert('Hubo un problema al enviar el correo. Intenta de nuevo.');
-      console.log(error.text);
-    });
-    console.log("Formulario enviado", formData);
+
+    const emailData = {
+      ...formData,
+      to_email: destinatario,
+    };
+
+    emailjs
+      .send(
+        "service_fcnnvtf",
+        "template_bwaptb9",
+        emailData,
+        "1sBpeKaGLYovogvqY"
+      )
+      .then(
+        (result) => {
+          alert("Correo enviado con éxito!");
+          console.log(result.text);
+          // Limpia el formulario
+          setFormData({ name: '', phone: '', email: '', comment: '' });
+        },
+        (error) => {
+          alert("Hubo un problema al enviar el correo. Intenta de nuevo.");
+          console.log(error.text);
+        }
+      );
+    console.log("Formulario enviado", emailData);
   };
 
   return (
@@ -102,7 +118,15 @@ const Contacto = () => {
             Enviar
           </button>
         </form>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9291.534435932665!2d-58.420864227677704!3d-34.57751272451957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb57ebaeac219%3A0xa485a3c2ba14616b!2sAv.%20Gral.%20Las%20Heras%203780%2C%20C1425ATO%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1727286180253!5m2!1ses!2sar" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9291.534435932665!2d-58.420864227677704!3d-34.57751272451957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb57ebaeac219%3A0xa485a3c2ba14616b!2sAv.%20Gral.%20Las%20Heras%203780%2C%20C1425ATO%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1727286180253!5m2!1ses!2sar"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
       <img className={styles.edificios} src={edificios} alt="edificios" />
     </div>
